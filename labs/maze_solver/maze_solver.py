d2d46a39608ec2c0019ff81523d58afaa5ceec68
ronwellman <ron.wellman01@gmail.com>
2019-11-29 16:32:57 -0500

    Complete class Solver basic functionality.

--- labs/maze_solver/maze_solver.py
+++ labs/maze_solver/maze_solver.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import time
+import os
 import sys
 from maze_support import Maze
 
@@ -11,22 +13,25 @@ class Solver:
         self.maze = maze
         self.start = maze.get_start()
         self.end = None
-        self.curr_row, self.curr_col = self.start
+        self.curr_pos = self.start
         self.visited = set()
         self.seen = set()
+        self.path = [self.start]
         self.visited.add(self.start)
         self.seen.add(self.start)
+        self.query_quadrants()
 
-    def search_quadrants(self):
+    def query_quadrants(self):
         '''
-        search_quadrants() -> NoneType
+        query_quadrants() -> NoneType
 
         Peers into the four coordinates around the current position.
         '''
-        self.update_seen(self.curr_row + 1, self.curr_col)
-        self.update_seen(self.curr_row - 1, self.curr_col)
-        self.update_seen(self.curr_row, self.curr_col + 1)
-        self.update_seen(self.curr_row, self.curr_col - 1)
+        row, col = self.curr_pos
+        self.update_seen(row + 1, col)
+        self.update_seen(row - 1, col)
+        self.update_seen(row, col + 1)
+        self.update_seen(row, col - 1)
 
     def update_seen(self, row, col):
         '''
@@ -35,29 +40,68 @@ class Solver:
         Checks the position and updates seen if position is empty.
         '''
         value = self.maze.get_point(row, col)
-        if value == ' ':
+
+        if value == ' ' or value == "E":
             self.seen.add((row, col))
-        elif value == 'E':
-            self.none((row, col))
+
+        if value == 'E':
+            self.end = (row, col)
+
+    def perform_search(self):
+        '''
+        perform_search() -> NoneType
+
+        Continues to move around until all all nodes have been visited.
+        '''
+        while True:
+            unvisited = self.seen.difference(self.visited)
+            # print(self.seen, self.visited, unvisited)
+            if len(unvisited) == 0:
+                break
+
+            row, col = self.curr_pos
+
+            # choose a direction to go
+            if (row + 1, col) in unvisited:
+                self._update_pos(row + 1, col)
+            elif (row - 1, col) in unvisited:
+                self._update_pos(row - 1, col)
+            elif (row, col + 1) in unvisited:
+                self._update_pos(row, col + 1)
+            elif (row, col - 1) in unvisited:
+                self._update_pos(row, col - 1)
+            else:
+                self._update_pos()
+
+            self.query_quadrants()
+            time.sleep(.1)
+            os.system("clear")
+            print(self)
+            if self.end is not None:
+                break
+
+    def _update_pos(self, row=None, col=None):
+        '''
+        update_pos(int, int) -> NoneType
+
+        Updates current position.
+        '''
+
+        if row is None or col is None:
+            self.curr_pos = self.path.pop()
+            return
+
+        self.curr_pos = (row, col)
+        self.path.append(self.curr_pos)
+        self.visited.add(self.curr_pos)
 
     def __str__(self):
-        min_row = min((row for row, col in self.seen))
-        min_col = min((col for row, col in self.seen))
-        max_row = max((row for row, col in self.seen))
-        max_col = max((col for row, col in self.seen))
-        print(min_row, max_row)
-        print(min_col, max_col)
-        visited = []
+        '''
+        __str__() -> str
 
-        for row in range(min_row, max_row + 1):
-            for col in range(min_col, max_col + 1):
-                if (row, col) in self.seen:
-                    visited.append(".")
-                else:
-                    visited.append(" ")
-            visited.append("\n")
-
-        return ''.join(visited)
+        Print the current state of the maze.
+        '''
+        return self.maze.__str__(self.visited, self.curr_pos)
 
 
 if __name__ == "__main__":
@@ -71,9 +115,5 @@ if __name__ == "__main__":
         print("Unable to load maze: {}".format(sys.argv[1]))
         sys.exit(0)
 
-    print(maze)
-
-    start = maze.get_start()
     solver = Solver(maze)
-    solver.search_quadrants()
-    print(solver)
+    solver.perform_search()
